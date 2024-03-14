7b31935b1c75886b02101751fbd5fb43852e372f
Ustinion <101933552+Ustinion@users.noreply.github.com>
2024-03-14 14:19:35 +0800

    Add files via upload

---
+++ a-high-concurrency-webserver-in-linux-master/a-high-concurrency-webserver-in-linux-master/WebServer/include/manager/file_manager.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+#include <unistd.h>
+#include <dirent.h>
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <memory>
+
+#include "locker/mutex_lock.h"
+#include "log/logging.h"
+
+
+namespace manager
+{
+    
+class FileManager {
+ public:
+    using fileManagerPtr = std::shared_ptr<FileManager>;
+    
+ public:
+    FileManager();
+    ~FileManager();
+
+    static fileManagerPtr getFileManager();
+    static std::vector<std::string> getFileList() { return fileList_; }
+    static std::string getFileName();
+
+
+ private:
+    static std::vector<std::string> fileList_;
+    static locker::MutexLock mutex_;
+    static fileManagerPtr instance_;
+};
+
+
+} // namespace manager
+
+
+
