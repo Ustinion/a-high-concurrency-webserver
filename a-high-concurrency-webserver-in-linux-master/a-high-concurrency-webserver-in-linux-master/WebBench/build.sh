ca02d7dee621e4c99bdd64c7c915b83f839d0ec2
Ustinion <101933552+Ustinion@users.noreply.github.com>
2024-03-23 19:00:55 +0800

    Delete a-high-concurrency-webserver-in-linux-master/a-high-concurrency-webserver-in-linux-master directory

--- a-high-concurrency-webserver-in-linux-master/a-high-concurrency-webserver-in-linux-master/WebBench/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#编译
-make clean && make -j8 && make install
-rm -f *.o
-
-
--- a-high-concurrency-webserver-in-linux-master/a-high-concurrency-webserver-in-linux-master/WebBench/web_bench.h
+++
@@ -1,8 +0,0 @@
-#ifndef WEB_BENCH_H_
-#define WEB_BENCH_H_
-
-void ParseArg(int argc, char* argv[]);
-void BuildRequest(const char* url);
-void WebBench();
-
-#endif  // WEB_BENCH_H_
